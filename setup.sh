39befe246dd64742189784386c4d9e973f429087
Blair Replogle <UpBra@users.noreply.github.com>
2024-04-17 14:47:10 -0600

    make force the default unless you specify to ask

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+results=()
 BOLD() { tput bold; }
 RED() { tput setaf 1; }
 GREEN() { tput setaf 2; }
@@ -7,13 +8,14 @@ NORMAL() { tput sgr0; }
 printInfo() { BOLD; printf "%s\n" "$1"; NORMAL; }
 printSuccess() { BOLD; GREEN; printf "%s\n" "$1"; NORMAL; }
 printError() { BOLD; RED; printf "%s\n" "$1"; NORMAL; }
+success() { printSuccess "$1"; results+=("$1"); }
 
 usage() {
 cat <<EOS
 Dev Setup
 Usage: setup.sh [options]
+	-a, --ask		Ask for confirmation on each step.
 	-h, --help		Display this message.
-	-f, --force		Force. Install without prompting for user input
 EOS
 exit "${1:-0}"
 }
@@ -21,8 +23,8 @@ exit "${1:-0}"
 while [[ $# -gt 0 ]]
 do
 	case "$1" in
+		-a | --ask) ask=true ;;
 		-h | --help) usage ;;
-		-f | --force) force=true ;;
 		*)
 			printError "Unrecognized option: '$1'"
 			usage 1
@@ -31,15 +33,19 @@ do
 	shift
 done
 
-# Methods
-
+# ---------------------------------------------------------------------
+# Checks if a command is installed
+# ---------------------------------------------------------------------
 check_install() {
 	type "$1" &> /dev/null
 }
 
+# ---------------------------------------------------------------------
+# Asks the user for confirmation (only if the ask flag is set)
+# ---------------------------------------------------------------------
 ask() {
-	# if force flag then don't ask. return yes
-	[[ -n ${force} ]] && return 0
+	# if ask flag is not set return yes
+	[[ -z ${ask} ]] && return 0
 
 	echo "$1"
 	select yn in "Yes" "No"; do
@@ -50,27 +56,32 @@ ask() {
 	done
 }
 
-ask_install() {
-	# if its already installed don't ask
-	if type "$1" &> /dev/null; then
+# ---------------------------------------------------------------------
+# Helper method to determine if the command is already installed
+# If this method returns success it should be installed
+# If this method returns an error do not install
+# ---------------------------------------------------------------------
+require_install() {
+	# if its already installed return no
+	if check_install "$1"; then
 		return 1
 	fi
 
 	ask "Do you wish to install $1?"
 }
 
-# Dot Zprofile
+# ---------------------------------------------------------------------
+# Create .zprofile if it doesn't already exist
+# ---------------------------------------------------------------------
 
 dotzprofile="${ZDOTDIR:-$HOME}/.zprofile"
 [ ! -f "$dotzprofile" ] && touch "$dotzprofile"
 
-# Start
-
-tput clear
-
+# ---------------------------------------------------------------------
 # Homebrew
+# ---------------------------------------------------------------------
 
-if ask_install "brew"; then
+if require_install "brew"; then
 	printInfo "Installing homebrew..."
 
 	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -84,14 +95,14 @@ if ask_install "brew"; then
 	# shellcheck disable=SC2016
 	echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> "$dotzprofile"
 
-	tput clear
-
-	printSuccess "Finished installing Homebrew"
+	success "Installed Homebrew!"
 fi
 
+# ---------------------------------------------------------------------
 # ASDF
+# ---------------------------------------------------------------------
 
-if ask_install "asdf"; then
+if require_install "asdf"; then
 	printInfo "Installing ASDF..."
 
 	brew install coreutils curl git asdf
@@ -107,7 +118,46 @@ if ask_install "asdf"; then
 	# shellcheck disable=SC1091
 	. "$(brew --prefix asdf)"/libexec/asdf.sh
 
-	tput clear
-
-	printSuccess "Finished installing ASDF"
+	success "Installed ASDF!"
 fi
+
+if ask "Do you want to install the asdf plugins?"; then
+	asdf plugin add awscli
+	success "Installed asdf plugin awscli"
+
+	brew install jq
+	asdf plugin add flutter
+	success "Installed asdf plugin flutter"
+
+	asdf plugin add java
+	success "Installed asdf plugin java"
+
+	asdf plugin add nodejs
+	success "Installed asdf plugin nodejs"
+
+	asdf plugin add python
+	success "Installed asdf plugin python"
+
+	asdf plugin add ruby
+	success "Installed asdf plugin ruby"
+
+	asdf plugin add terraform
+	success "Installed asdf plugin terraform"
+fi
+
+# ---------------------------------------------------------------------
+# Summary
+# ---------------------------------------------------------------------
+
+tput clear
+
+printInfo "Summary"
+for value in "${results[@]}"
+do
+	printSuccess "$value"
+done
+
+printInfo ""
+printError "Quit and re-open your terminal for the changes to take affect!"
+
+exit 0
